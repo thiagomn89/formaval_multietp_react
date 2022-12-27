bb9bf8e7549bba18a33c0da3e2f968e2cf110a55
thiagomn89 <thiagomagalhaes.oficial@gmail.com>
2022-12-27 15:33:28 -0300

    formaval_multietp_react

---
+++ src/hooks/useform.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export function useForm(steps) {
+    const [currentStep, setCurrentStep] = useState(0)
+
+    // limitando as páginas
+
+    function changeStep(i, e) {
+        if (e) e.preventDefault();
+
+        if(i < 0 || i >= steps.length) return
+
+        setCurrentStep(i);
+    }
+
+    return {
+        currentStep,
+        currentComponent: steps[currentStep],
+        changeStep,
+        isLastStep: currentStep + 1 === steps.length ? true : false,
+        isFirstStep: currentStep === 0 ? true : false,
+    };
+
+}
